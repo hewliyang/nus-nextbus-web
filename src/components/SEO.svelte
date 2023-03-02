2de3d6e9ea9cdda61630c10d915e1f889e4da53a
hewliyang <hewliyang@u.nus.edu>
2023-03-02 10:08:53 +0800

    google verification

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -9,6 +9,7 @@
     <meta name="description" content="A faster way of getting your ISB timings!">
     <meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
     <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000000" />
+    <meta name="google-site-verification" content="X5Gix4InPC8naCq_jdp43uouWw7_pekxQEgoZbAIBbc" />
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="icon" href="/favicon.png" type="image/png">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
