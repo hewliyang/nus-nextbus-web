8d3a75b2377901ef73a739a4c18c5475e877799d
hewliyang <hewliyang@u.nus.edu>
2023-07-08 00:10:26 +0800

    bugfix: info svg color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,10 +47,21 @@
 	<div class="alert alert-info text-sm">
 		<div class="flex-1 justify-between">
 			<div class="flex gap-2 items-center">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6 text-cyan-500"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="stroke-current shrink-0 w-6 h-6"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
 				<span class="font-bold">Be sure to bookmark🔖 your frequent stops for easy access!</span>
 			</div>
-			<form action="?/closeAlert" method='POST' use:enhance>
+			<form action="?/closeAlert" method="POST" use:enhance>
 				<button class="btn btn-outline btn-sm">X</button>
 			</form>
 		</div>
